29d906ae1c105edbb75c5970d94cbe42c96d2357
Calvin <calvinhlee23@gmail.com>
2016-08-23 19:58:17 -0700

    weather app done

--- weather.jsx
+++ weather.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 
 class Weather extends React.Component {
-  contructor(prop) {
-    super(prop);
-    this.
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: '',
+      temp: ''
+    };
+
+    this.getLocationInfo = this.getLocationInfo.bind(this);
+
+  }
+
+
+  getLocationInfo () {
+    navigator.geolocation.getCurrentPosition((success) => {
+      this.setState({location: success.coords});
+    });
+    this.lat = this.state.location.latitude;
+    this.longi = this.state.location.longitude;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=`;
+    url += `${this.lat}&lon=${this.longi}`;
+    url += `&&APPID=645c5d39c7603f17e23fcaffcea1a3c1`;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+
+    request.onreadystatechange = () => {
+      if (request.status === 200 && request.readyState === XMLHttpRequest.DONE) {
+        const data = JSON.parse(request.responseText);
+        this.setState({location: data.name});
+        this.setState({temp: (data.main.temp- 273.15) * 1.8 + 32});
+      }
+    };
+    request.send();
+
+  }
+
+  componentDidMount() {
+    this.getLocationInfo();
+  }
+
+
+  render () {
+    if (this.location) {
+      return (<a>getting info</a>);
+    }
+    else {
+
+      return (
+        <div className = 'Weather'>
+        <h1 className = 'title'>Weather</h1>
+        <a className = "weather-temp">
+        Temperature: {Math.floor(this.state.temp)} F</a>
+        <br/>
+        <a className = "weather-city">
+        Location: {this.state.location}</a>
+        </div>
+      );
+    }
   }
 }
+
+export default Weather;
--- widgets.jsx
+++ widgets.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import Tabs from './tabs';
 import Clock from './clock';
+import Weather from './weather';
+
 const tabs = [
   { title: "one",
     content: "this is content of one"
@@ -25,5 +27,19 @@ function success (pos) {
 }
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Clock/>, root);
+  ReactDOM.render(<Weather/>, root);
 });
+
+
+// 
+// {"coord":{"lon":-117.93,"lat":33.87},
+// "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],
+// "base":"cmc stations",
+// "main":{"temp":299.25,"pressure":1015,"humidity":44,"temp_min":293.71,"temp_max":304.15},
+// "wind":{"speed":2.6},
+// "clouds":{"all":1},
+// "dt":1472003206,
+// "sys":{"type":1,"id":390,"message":0.0055,"country":"US","sunrise":1472044822,"sunset":1472092008},
+// "id":5351247,
+// "name":"Fullerton",
+// "cod":200}
